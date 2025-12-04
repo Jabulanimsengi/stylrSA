d7d87321b0795e3cd2127ce50baad5d291b67bd1
Jabulani <tsakanimsengi@gmail.com>
2025-12-04 21:49:38 +0200

    Fix open-next config with edgeExternals

--- frontend/open-next.config.ts
+++ frontend/open-next.config.ts
@@ -12,12 +12,16 @@ const config = {
       queue: "dummy",
     },
   },
+  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
     override: {
       wrapper: "cloudflare-edge",
       converter: "edge",
       proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 };
